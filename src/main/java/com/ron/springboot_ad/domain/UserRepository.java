d3ed4d8f4af86719f49e20edc35980ef68dbb2af
Ron <a3300552@hpicorp.com.tw>
2020-07-22 18:04:07 +0800

    32
    32 table not ok

---
+++ src/main/java/com/ron/springboot_ad/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package com.ron.springboot_ad.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
---
+++ src/main/java/com/ron/springboot_ad/web/LoginController.java
@@ -0,0 +1,40 @@
+package com.ron.springboot_ad.web;
+
+import com.ron.springboot_ad.domain.User;
+import com.ron.springboot_ad.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String username,
+                               @RequestParam String password,
+                               @RequestParam String email,
+                               @RequestParam Integer phone) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setPhone(phone);
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
