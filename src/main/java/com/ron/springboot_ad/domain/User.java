c1ae3c978002195c48ef291577a21ba21692c4c2
a3300552 <a3300552@hpicorp.com.tw>
2020-07-22 23:35:04 +0800

    32
    32 ok
    更換設定檔

--- src/main/java/com/ron/springboot_ad/domain/User.java
+++ src/main/java/com/ron/springboot_ad/domain/User.java
@@ -1,15 +1,16 @@
 package com.ron.springboot_ad.domain;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
